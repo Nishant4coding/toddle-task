2edffcbc638a5b7e0dc3a0d353f712c127f5a16c
Avanya Tyagi <118192556+avanyatyagi@users.noreply.github.com>
2024-06-02 12:13:46 +0530

    Update Foooter.js

--- src/components/Foooter.js
+++ src/components/Foooter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-<footer className="bg-white font-bold text-xl text-[#008392] py-4 text-center fixed bottom-0 w-full">
+<footer className="bg-white font-bold text-lg text-[#008392] py-4 text-center fixed bottom-0 w-full">
   <p className="">Designed and Developed by <a href="https://www.linkedin.com/in/avanyatyagi08/" className=''> Avanya Tyagi</a></p>
 </footer>
 
